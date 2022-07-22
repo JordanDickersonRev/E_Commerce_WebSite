3c7264d3bfcc9acb3bc5885146448d635497345a
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-07-22 00:36:22 -0400

    layout for home page is done

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,8 +2,32 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return(
-        <div>  
+        <div className='homePage'>
+            <Link to='/store'>
+                <div className='homeSection1'>
+                    <h2 className='hs1Heading'>BEST DEALS FOR COMPLETE SKATEBOARDS</h2>
+                    <label className='hs1Label'>SHOP NOW!</label>
+                </div>
+            </Link>
+            <div className='homeSection2'>
+                
+            </div>
+            <div className='homeSection3'>
+                <div className='homeCard1'>
+                    
+                </div>
+                <div className='homeCard2'>
+                    
+                </div>
+                <div className='homeCard3'>
+
+                </div>
+                <div className='homeCard4'>
+
+                </div>
+            </div>  
         </div>
     )
 }
 export default Home;
+/*<img src={require('./branding/decksImage.png')} alt='SKATE DECKS'/>*/
