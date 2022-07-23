d9bc558067baee432be323b285c871e24fb8947d
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-07-22 21:37:05 -0400

    half way done with home page html and css

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,27 +3,33 @@ import { Link } from 'react-router-dom';
 function Home() {
     return(
         <div className='homePage'>
-            <Link to='/store'>
+            <Link to='/store' className='homeLink'>
                 <div className='homeSection1'>
-                    <h2 className='hs1Heading'>BEST DEALS FOR COMPLETE SKATEBOARDS</h2>
-                    <label className='hs1Label'>SHOP NOW!</label>
+                    <img src={require('./branding/decksImage.png')} alt='SKATE DECKS'/>
+                    <button>SHOP NOW!</button>
+                    <span/>
                 </div>
             </Link>
             <div className='homeSection2'>
-                
+                <img src={require('./branding/skateLedge.png')} alt='SKATE LEDGE'/>
+                <button>ENTER TO WIN OUR SKATEHOUSE LEDGE W/ FULL SEND WAX</button>
             </div>
             <div className='homeSection3'>
                 <div className='homeCard1'>
-                    
+                    <img src={require('./branding/twitter.png')} alt='TWITTER'/>
+                    <button>FOLLOW US ON TWITTER</button>
                 </div>
                 <div className='homeCard2'>
-                    
+                    <img src={require('./branding/instagram.png')} alt='INSTAGRAM'/>
+                    <button>FOLLOW US ON INSTAGRAM</button>
                 </div>
                 <div className='homeCard3'>
-
+                    <img src={require('./branding/snapchat.png')} alt='SNAPCHAT'/>
+                    <button>FOLLOW US ON SNAPCHAT</button>
                 </div>
                 <div className='homeCard4'>
-
+                    <img src={require('./branding/facebook.png')} alt='FACEBOOK'/>
+                    <button>FOLLOW US ON FACEBOOK</button>
                 </div>
             </div>  
         </div>
@@ -31,3 +37,5 @@ function Home() {
 }
 export default Home;
 /*<img src={require('./branding/decksImage.png')} alt='SKATE DECKS'/>*/
+/*<h2>BEST DEALS FOR COMPLETE SKATEBOARDS</h2>
+                    */
