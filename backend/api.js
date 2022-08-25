46c6ac39d2655730d8f3d35ea75c0bcbfd6251e1
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-08-25 00:34:02 -0400

    removed body parser and add size hr width in css

--- backend/api.js
+++ backend/api.js
@@ -1,13 +1,12 @@
 const database = require('./database');
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 3001;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/store", (req,res)=>{
 
